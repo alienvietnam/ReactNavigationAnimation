a62f2612a833ec089ad431cd32e207a0e904ff45
WhoIam <phamtienvung@gmail.com>
2018-12-17 12:08:08 +0700

    aaa

--- src/components/SlideView.js
+++ src/components/SlideView.js
@@ -1,34 +1,56 @@
 /* eslint-disable quotes */
 import React, { Component } from "react";
-import { Text, Animated } from "react-native";
+import { View, Text, Animated, Easing } from "react-native";
 
 class SlideView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      slideAnim: new Animated.Value(-700)
+      slideAnim1: new Animated.Value(-700),
+      slideAnim2: new Animated.Value(-700)
     };
   }
 
   componentDidMount() {
-    Animated.timing(this.state.slideAnim, {
+    const anim1 = Animated.timing(this.state.slideAnim1, {
       toValue: 0,
-      duration: 5000
-    }).start();
+      duration: 2000,
+      easing: Easing.bounce
+    });
+    const anim2 = Animated.timing(this.state.slideAnim2, {
+      toValue: 0,
+      duration: 2000,
+      easing: Easing.bounce
+    });
+
+    Animated.stagger(300, [anim1, anim2]).start();
   }
   render() {
-    const marginLeft = this.state.slideAnim;
+    const marginLeft1 = this.state.slideAnim1;
+    const marginLeft2 = this.state.slideAnim2;
     return (
-      <Animated.View
-        style={{
-          width: 300,
-          height: 200,
-          backgroundColor: "green",
-          marginLeft
-        }}
-      >
-        <Text>Animated Demo</Text>
-      </Animated.View>
+      <View>
+        <Animated.View
+          style={{
+            width: 300,
+            height: 200,
+            backgroundColor: "green",
+            marginLeft: marginLeft1
+          }}
+        >
+          <Text>Animated Demo</Text>
+        </Animated.View>
+        <Animated.View
+          style={{
+            width: 300,
+            height: 200,
+            backgroundColor: "red",
+            marginLeft: marginLeft2
+          }}
+        >
+          <Text>Animated Demo</Text>
+        </Animated.View>
+      </View>
     );
   }
 }
